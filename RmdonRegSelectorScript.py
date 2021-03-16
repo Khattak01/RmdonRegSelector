15f73869d9b3dfeebba4146aebe77b37398157e9
Khattak01 <57499748+Khattak01@users.noreply.github.com>
2021-03-16 15:11:02 +0500

    Add files via upload

---
+++ RmdonRegSelectorScript.py
@@ -0,0 +1,26 @@
+import sys
+import random
+#print("Hello, World")
+#print(sys.version)
+stdReg = {
+    0:'FA17-BSE-(103,131,160)',
+    1:'FA17-BSE (106,108,142)',
+    2:'FA17-BSE-(104,081)',
+    3:'FA17-BSE-(105,109,138)',
+    4:'FA17-BSE-(130,107)',
+    5:'Fa17-BSE-(137,123)',
+    6:'FA17-BSE-(102,149)',
+    7:'FA17-BSE-(145,152,143)',
+    }
+def printStd(dic):
+    for item in dic:
+        print(dic[item])
+print('Orignal List')
+printStd(stdReg)
+print('-------------------------')
+print('Randomly generated LIST : ')
+randomList =(random.sample(range(0, len(stdReg)), len(stdReg)))
+num = 0
+for i in randomList:
+    print("Serial number : "+str(num)+"\n \t"+stdReg[i])
+    num+=1
